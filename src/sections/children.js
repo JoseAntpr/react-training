0174b9a03f3a7ddf4a9671bce4503863e8aac6d0
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-07-25 11:06:15 +0200

    ADD: add children layout

--- src/sections/children.js
+++ src/sections/children.js
@@ -10,13 +10,36 @@ class Box extends Component {
     }
 }
 
+class Article extends Component {
+    render() {
+        return (
+            <section>
+                <h2>{this.props.title}</h2>
+                <p><em>Escrito por {this.props.author}</em></p>
+                <Box>{this.props.date}</Box>
+                <article>
+                    {this.props.children}
+                </article>
+            </section>
+        )
+        
+    }
+}
+
 export default class Children extends Component {
     render() {
         return (
             <div>
                 <h4>Children props</h4>
-                <Box>Hola! Soy un children</Box>
-                <Box>Otro box con otro contenido</Box>
+                <Article
+                    author="Joseantpr"
+                    date={new Date().toLocaleDateString()}
+                    title='Articulo de prop children'
+                >
+                    <p>El contenido que envolvemos dentro del componente Article será enviado como 
+                    <strong> this.props.children</strong>
+                    </p>
+                </Article>
             </div>
         )
     }
