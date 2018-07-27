be10f4560ec9fc4880d1e414e512674b1ba44c18
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-07-27 11:51:03 +0200

    ADD: shouldComponent update and componen will update implemented

--- src/sections/life-cycle/updateLifeCyleExample.js
+++ src/sections/life-cycle/updateLifeCyleExample.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 const ANIMAL_IMAGES = {
@@ -14,9 +14,34 @@ class AnimaImage extends Component{
     state = { src: ANIMAL_IMAGES[this.props.animal] }
 
     componentWillReceiveProps( nextProps) {
-        console.log('ComponentWillReceiveProps', nextProps);
+        console.log('1. ComponentWillReceiveProps', nextProps);
         this.setState({ src: ANIMAL_IMAGES[nextProps.animal]})
+    }
 
+    shouldComponentUpdate(nextProps) {
+        /* Hay que devolver un booleano,
+        Si este método no existe devuelve true por defecto */
+        console.log('2. shouldComponentUpdate');
+        console.log('Anterior', this.props.animal);
+        console.log('Nuevos', nextProps.animal);
+        return this.props.animal !== nextProps.animal;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('3. ComponentWillUpdate', nextProps, nextState);
+        const img = document.querySelector('img');
+        console.log('From img element', {alt: img.alt});
+        // Web animations api
+        img.animate([
+            {
+                filter: 'blur(0px)'
+            },
+            {
+                filter: 'blur(2px)'
+            }],{
+                duration: 500,
+                easing: 'ease'
+            })
     }
 
     render() {
@@ -45,14 +70,16 @@ export default class UpdateLifeCycleExample extends Component {
         return <button 
                     key={animal} 
                     onClick={() => this.setState({ animal })} 
-                    disabled={animal === this.state.animal}>
+                    //disabled={animal === this.state.animal}
+                    >
                     {animal}
                 </button>
     }
     render() {
+        console.log('4. render');
         return(
             <div>
-                <h4> Ciclo de Actualización, EJemplo de : COmponentWillReceiveProps</h4>
+                <h4> Ciclo de Actualización, EJemplo de : ShouldComponentUpdate</h4>
                 {ANIMALS.map( this._renderAnimalButton)}
                 <AnimaImage animal={this.state.animal} />
             </div>
